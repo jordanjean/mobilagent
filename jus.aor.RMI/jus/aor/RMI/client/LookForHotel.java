15a0c98c93fb54d04dc78e920221aa8a488a660d
leovalette <leovalette38@gmail.com>
2018-03-22 09:10:45 +0100

    LookForHotel + Server

--- jus.aor.RMI/jus/aor/RMI/client/LookForHotel.java
+++ jus.aor.RMI/jus/aor/RMI/client/LookForHotel.java
@@ -1,11 +1,23 @@
 package jus.aor.RMI.client;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
+
+import jus.aor.RMI.common.Hotel;
+import jus.aor.RMI.common._Annuaire;
+import jus.aor.RMI.common._Chaine;
 
 public class LookForHotel {
 	/** le critère de localisaton choisi */
 	private String localisation;
 	private int port = 1099;
+	private _Annuaire annuaire;
+	private int nbChaines = 6;
+	private List<Hotel> hotels = new ArrayList<Hotel>();
 
 	// ...
 	/**
@@ -28,18 +40,40 @@ public class LookForHotel {
 	}
 
 	/**
-	 * réalise une intérrogation
+	 * réalise une interrogation
 	 * 
 	 * @return la durée de l'interrogation
 	 * @throws RemoteException
+	 * @throws NotBoundException
 	 */
-	public long call() throws RemoteException {
+	public long call() throws RemoteException, NotBoundException {
 		long td = System.currentTimeMillis();
 
-		return System.currentTimeMillis() - td;
+		Registry registre = null;
+
+		// On parcourt les chaines hotelières et on regarde dans leur
+		// registre à leur port les hotels qu'elle possède à la localisation
+		for (int i = 1; i <= this.nbChaines; i++) {
+			registre = LocateRegistry.getRegistry(this.port + i);
+			this.hotels.addAll((List<Hotel>) ((_Chaine) registre.lookup("chaine" + i)).get(this.localisation));
+		}
+
+		this.annuaire = (_Annuaire) registre.lookup("annuaire");
+
+		// On arrête le timer ici, l'affichage ne fait pas partie du temps de
+		// recherche
+		long tf = System.currentTimeMillis();
+
+		// Affichage des hotels trouvés
+		System.out.println("Hotels :");
+		for (Hotel h : hotels) {
+			System.out.println(h.toString());
+		}
+
+		return tf - td;
 	}
 
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws RemoteException, NotBoundException {
 		LookForHotel lfh = new LookForHotel(args);
 		long time = lfh.call();
 		System.out.println("Research done in " + time + " milliseconds");
--- jus.aor.RMI/jus/aor/RMI/server/Server.java
+++ jus.aor.RMI/jus/aor/RMI/server/Server.java
@@ -1,5 +1,40 @@
 package jus.aor.RMI.server;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Server {
+
+	public static void main(String[] args) {
+
+		int port = 1099;
+		int nbChaines = 6;
+
+		if (args.length == 1) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (Exception e) {
+				System.out.println("Erreur : Pas de numero de port valide.");
+				System.exit(1);
+			}
+		}
+
+		// installation d'un securityManager
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+
+		Registry registre;
+		for (int i = 1; i <= nbChaines; i++) {
+			registre = LocateRegistry.createRegistry(port + i);
+			Chaine string = new Chaine("../DataStore/Hotels" + i + ".xml");
+			registre.bind("chaine" + i, string);
+		}
+		registre = LocateRegistry.createRegistry(port + nbChaines + 1);
+		Annuaire a = new Annuaire("DataStore/Annuaire.xml");
+		registre.bind("annuaire", a);
+
+		System.out.println("Démarrage du serveur effectué !");
+	}
 
 }
